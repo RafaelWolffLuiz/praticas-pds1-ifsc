fb016fda40ce2be0912491d277dfec58086e2579
RafaelWolffLuiz <126568580+RafaelWolffLuiz@users.noreply.github.com>
2023-03-01 17:02:21 -0300

    Add files via upload

---
+++ lista1/src/lista1/Exercicio1.java
@@ -0,0 +1,52 @@
+package lista1;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		// NUMERICOS
+		// inteiros = int (4 números = 4000 5000 6000), long (8 Números, mesma coisa da anterior)   
+		// reais (decimais) = float (4 números = 0,1234 , 0,2564
+		//                    Double (8 números = 0,35783423 , 0,23245678
+		// ALFANUMERICO (letra, frase...)
+		// char = letra
+		// String = frase
+		
+		// Permite a leitura
+		Scanner leitura = new Scanner(System.in);	
+		/*System.out.println("Informe Seu Nome Completo:");
+		String nome = leitura.nextLine();
+		System.out.println("Seu nome completo é: " + nome);
+		System.out.println("Informe seu primeiro nome:");
+		String Pnome = leitura.nextLine();
+		System.out.println("Seu primeiro nome é:" + Pnome);*/
+		
+		  /*System.out.println("Digite a sua idade:");
+		int idade = leitura.nextInt();
+		System.out.println("Sua idade é:" + idade);
+		
+		System.out.println("Digite a sua altura:");
+		double altura = leitura.nextDouble(); 
+		System.out.println("Sua altura é:" + altura); */
+		
+		double num1 = 40;
+		double num2 = 60;
+		double resultado;	
+		resultado = num1 / num2;
+		
+		System.out.println("O Resultado da divisão é:" + resultado);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
