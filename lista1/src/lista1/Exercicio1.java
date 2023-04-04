b83c59fb986184b09cf599f96e62040554b8c214
RafaelWolffLuiz <126568580+RafaelWolffLuiz@users.noreply.github.com>
2023-04-03 22:14:31 -0300

    Update Exercicio1.java

--- lista1/src/lista1/Exercicio1.java
+++ lista1/src/lista1/Exercicio1.java
@@ -1,52 +1,62 @@
 package lista1;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+   // Utilizei, CHATGPT para poder compreender como fazer e aprender, YT e alguns sites e também ajuda de amigos,
+   // tenho muita dificuldade mas estou sendo sincero. Caio me ajudando entrando call discord.
+public class CalctotulPag {
+public static void main(String[] args) {
+       
+  double PrecoLitro = Double.parseDouble(JOptionPane.showInputDialog("Preço por litro:"));
+    int quantLitros = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de litros:"));
+        
+        
+  double totalPag = calctotulPag(PrecoLitro, quantLitros);
+        JOptionPane.showMessageDialog(null, "O total a ser pagu é R$:" + totalPag);
+    
+public static double calctotulPag(double PrecoLitro, int quantLitros) {
+return PrecoLitro * quantLitros;
+    }
+    }
+}
 
-public class Exercicio1 {
+//
+//
+//
 
-	public static void main(String[] args) {
-		
-		// NUMERICOS
-		// inteiros = int (4 números = 4000 5000 6000), long (8 Números, mesma coisa da anterior)   
-		// reais (decimais) = float (4 números = 0,1234 , 0,2564
-		//                    Double (8 números = 0,35783423 , 0,23245678
-		// ALFANUMERICO (letra, frase...)
-		// char = letra
-		// String = frase
-		
-		// Permite a leitura
-		Scanner leitura = new Scanner(System.in);	
-		/*System.out.println("Informe Seu Nome Completo:");
-		String nome = leitura.nextLine();
-		System.out.println("Seu nome completo é: " + nome);
-		System.out.println("Informe seu primeiro nome:");
-		String Pnome = leitura.nextLine();
-		System.out.println("Seu primeiro nome é:" + Pnome);*/
-		
-		  /*System.out.println("Digite a sua idade:");
-		int idade = leitura.nextInt();
-		System.out.println("Sua idade é:" + idade);
-		
-		System.out.println("Digite a sua altura:");
-		double altura = leitura.nextDouble(); 
-		System.out.println("Sua altura é:" + altura); */
-		
-		double num1 = 40;
-		double num2 = 60;
-		double resultado;	
-		resultado = num1 / num2;
-		
-		System.out.println("O Resultado da divisão é:" + resultado);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+public class StringsList {
+public static void main(String[] args) {
+   ArrayList<String> lista = new ArrayList<String>();
+        
+for (int i = 0; i < 10; i++) {
+     String elemento = JOptionPane.showInputDialog("Digite um elemento:");
+   lista.add(elemento);
+       
+   JOptionPane.showMessageDialog(null, "A lista é: " + lista);
+	 }
+    }
+}
 
+//
+//
+//
+
+public class Calmedia {
+public static void main(String[] args) {
+      double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Informe a primeira nota:"));
+      double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Informe a segunda nota:" ));
+      double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Informe a terceira nota:"));
+        
+        // Calculo da média
+      double LaMediaFinale = (nota1 + nota2 + nota3) / 3;
+        
+        //      Resultado 
+    if (LaMediaFinale >= 6) {
+       JOptionPane.showMessageDialog(null, "Aprovado(a)");
+  } else if (LaMediaFinale >= 4) {
+       JOptionPane.showMessageDialog(null, "Recuperação");
+  } else {
+       JOptionPane.showMessageDialog(null, "Reprovado(a)");
+        }
+    }
 }
